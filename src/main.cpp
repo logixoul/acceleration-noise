9825db21a93b897c17169a82099bcf2cb64f0236
Stefan Monov <logixoul@gmail.com>
2024-02-11 19:07:04 +0200

    blurworld works

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@
 #include <opencv2/videoio.hpp>
 #include "CrossThreadCallQueue.h"
 
-int wsx = 1280, wsy = 720;
-int scale = 6;
+int wsx = 600, wsy = 400;
+int scale = 3;
 int sx = wsx / ::scale;
 int sy = wsy / ::scale;
 Array2D<float> img(sx, sy);
@@ -62,82 +62,12 @@ struct SApp : App {
 		}
 	}
 	void stefanUpdate() {
-		if (!pause2) {
-			float lerpAmount = cfg1::getOpt("lerpAmount", .01f, []() { return keys['l']; },
-				[&]() { return expRange(constrain(mouseX, 0.0f, 1.0f), .0001f, 1.0f); });
-			float blurAmount = cfg1::getOpt("blurAmount", 0.40f, []() { return keys['b']; },
-				[&]() { return expRange(constrain(mouseX, 0.0f, 1.0f), .001f, 30.0f); });
-			//float lerpAmount = lerp(0.001, 0.009, pow(s1(getElapsedSeconds() * 0.6f), 3.0f));
-			//cout << lerpAmount << endl;
-			//img = separableConvolve<float, WrapModes::DefaultImpl>(img, getGaussianKernel(5, sigmaFromKsize(blurAmount)));
-			img = separableConvolve<float, WrapModes::DefaultImpl>(img, getGaussianKernel(3, blurAmount));
-			float sum = ::accumulate(img.begin(), img.end(), 0.0f);
-			float avg = sum / (float)img.area;
-			forxy(img)
-			{
-				float f = img(p);
-				f += .5f - avg;
-				f -= .5f;
-				//f *= 2.0f; // change to *= 1.1f for another effect
-				f += .5f;
-				f = constrain(f, 0.0f, 1.0f);
-				img(p) = f;
-			}
-			float sum_ = ::accumulate(img.begin(), img.end(), 0.0f);
-			float avg_ = sum_ / (float)img.area;
-			int r = 5;
-			Array2D<float> weights(r * 2 + 1, r * 2 + 1);
-			float sumw = 0.0f;
-			forxy(weights) {
-				weights(p) = smoothstep<float>(r, r - 1, distance(vec2(p), vec2(r, r)));
-				sumw += weights(p);
-			}
-			forxy(varianceArr)
-			{
-				float sum = 0.0f;
-				for (int i = -r; i <= r; i++)
-				{
-					for (int j = -r; j <= r; j++)
-					{
-						float w = weights(i + r, j + r);
-						sum += img.wr(p.x + i, p.y + j) * w;
-					}
-				}
-				float avg = sum / sumw;
-				float variance = 0.0f;
-				for (int i = -r; i <= r; i++)
-				{
-					for (int j = -r; j <= r; j++)
-					{
-						float w = weights(i + r, j + r);
-						variance += w * abs(img.wr(p.x + i, p.y + j) - avg);
-					}
-				}
-				if (avg == 0.0f)
-					varianceArr(p) = 0.0f;
-				else
-					varianceArr(p) = variance / avg;
-			}
-			forxy(img)
-			{
-				/*img(p) = lerp(img(p), varianceArr(p),
-					lerpAmount
-				);*/
-				img(p) += varianceArr(p) * lerpAmount;
-			}
-			auto img2 = zeros_like(img);
-			vec2 center(img.Size() / 2);
-			forxy(img) {
-				vec2 fromCenter = vec2(p) - center;
-				vec2 v(fromCenter.y, -fromCenter.x);
-				v *= .01f;
-				v = safeNormalized(v) * pow(length(v), 2.0f);
-				aaPoint(img2, vec2(p) + v, img(p));
-			}
-			//img = img2;
-		} // pause2
-		//if (pause || pause2)
-			//Sleep(50);
+		if (pause2)
+			return;
+		img = ::gaussianBlur(img, 3);
+		forxy(img) {
+			img(p) = smoothstep(0.0f, 1.0f, img(p));
+		}
 	}
 	void stefanDraw()
 	{
@@ -189,7 +119,9 @@ struct SApp : App {
 		}
 
 		auto tex = gtex(img3);
-		if(0)tex = shade2(tex,
+		tex = redToLuminance(tex);
+
+		/*if(0)tex = shade2(tex,
 			"float f = fetch1();"
 			"float fw = fwidth(f);"
 			//"f = smoothstep(.5 - fw / 2, 0.5 + fw / 2, f);"
@@ -200,7 +132,7 @@ struct SApp : App {
 		tex = redToLuminance(tex);
 		auto mat = dlToMat3(tex, 0);
 		mat.convertTo(mat, CV_8UC3, 255.0f);
-		mVideoWriter.write(mat);
+		mVideoWriter.write(mat);*/
 		gl::draw(tex, getWindowBounds());
 	}
 	void cleanup() override {
